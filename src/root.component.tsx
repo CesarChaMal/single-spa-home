aafcc7495dd25fbf0f0ab51b028a678ea4000c54
Cesar Chavez <cesarchanalu@gmail.com>
2024-07-16 02:06:43 +0200

    Update.

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -5,31 +5,56 @@ import parse from 'html-react-parser';
 
 export default function Root(props) {
 
-  var thisIsMyHome = '<div className="jumbotron">';
-  thisIsMyHome += '<h1 className="display-4">Hello, world!</h1>';
-  thisIsMyHome += '<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>';
-  thisIsMyHome += '<hr className="my-4">';
-  thisIsMyHome += '<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>';
-  thisIsMyHome += '<p className="lead">';
-  thisIsMyHome += '  <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>';
-  thisIsMyHome += '</p>';
-  thisIsMyHome += '</div>';
-  thisIsMyHome += '<em>{{props.name}} using React</em>';
-  // return <section>{props.name} is mounted!</section>;
+  // var thisIsMyHome = '<div className="jumbotron">';
+  // thisIsMyHome += '<h1 className="display-4">Hello, world!</h1>';
+  // thisIsMyHome += '<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>';
+  // thisIsMyHome += '<hr className="my-4">';
+  // thisIsMyHome += '<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>';
+  // thisIsMyHome += '<p className="lead">';
+  // thisIsMyHome += '  <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>';
+  // thisIsMyHome += '</p>';
+  // thisIsMyHome += '</div>';
+  // thisIsMyHome += '<em>{{props.name}} using React</em>';
 
-  return (
-    <div className="content">{parse(thisIsMyHome)}</div>
-  );
+  const thisIsMyHome = `
+  <div class="jumbotron">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">
+      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+    </p>
+    <hr class="my-4">
+    <p>
+      It uses utility classes for typography and spacing to space content out within the larger container.
+    </p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+  </div>
+  <em>${props.name} using React</em>
+`;
 
   // return (
-  //   <div className="jumbotron">
-  //     <h1 classNam="display-4">Hello, world!</h1>
-  //     <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  //     <hr className="my-4">
-  //     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  //     <p className="lead">
-  //       <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  //     </p>
-  //   </div>
+  //   <div className="content">{parse(thisIsMyHome)}</div>
   // );
+
+
+  return (
+    <div className="content">
+      <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">
+          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+        </p>
+        <hr className="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content out within the larger container.
+        </p>
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+      </div>
+      <em>{props.name} using React</em>
+    </div>
+  );
+
 }
